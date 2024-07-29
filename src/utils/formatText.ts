3da00b3a968c5bbf62121967d057333ca28c01f2
michael-jobin <jobinmic@gmail.com>
2024-07-29 11:06:43 +0900

    added the 3 last interviews

--- src/utils/formatText.ts
+++ src/utils/formatText.ts
@@ -1,3 +1,10 @@
 export function convertlinesBreaks(text: string): string {
  return text.replace(/\n/g, '<br>');
 }
+
+export function truncate(text: string, length: number): string {
+ if (text.length <= length) {
+  return text;
+ }
+ return text.slice(0, length) + '...';
+}
